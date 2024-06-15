c31714c9a8f4adcf3bbb9bbdd57e62468f9df6e4
urmilateli <teliurmila12@gmail.com>
2024-06-15 19:50:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+    window.location.href = "https://partyrock.aws/u/deepadhakar/BwNpWK5Lq/Pathfinder%3A-Guiding-Science-Graduates-to-College-and-Career-Success";
+});
